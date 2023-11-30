4bb6ef675b049b3432256708412db7f8ed90b239
Shawn Zhou <shaohengzhou@gmail.com>
2023-11-29 22:04:29 -0800

    requirement

--- batch_inference_frozen.sh
+++ batch_inference_frozen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sample_cnt=12
+sample_cnt=3
 for index in $sample_cnt
 do
     per_file_suffix=dog"$index"_frozen
-    eval "python inference.py --prompt "A dog" --model ./models/model_scope_diffusers --sdp --suffix $per_file_suffix --output-dir ./output/"
+    eval "python3 inference.py --prompt \"A dog\" --model ./models/model_scope_diffusers --sdp --suffix $per_file_suffix --output-dir ./output/"
 done
